b9c5d1edad64c90949b53f7bc5c76d95fff31faf
maricaBog <tvorac22@gmail.com>
2017-01-05 15:55:18 +0100

    dodata je rotacija objekta

--- makefile
+++
@@ -1,9 +0,0 @@
-projekat:projekat.c
-	gcc projekat.c -lglut -lGL -lGLU -o projekat
-	./projekat
-
-.PHONY: all projekat clean
-
-clean:
-	rm -rf *.c projekat
-all:	projekat
--- projekat.c
+++ projekat.c
@@ -1,19 +1,22 @@
 #include<GL/glut.h>
+#include<math.h>
+#include<stdlib.h>
+
+//vreme proteklo od pocetka simulacije
+static float hours;
+
+//fleg za stajanje tajmera
+static int timer_active;
 
 /*deklaracija funkcije*/
 static void on_display(void);
 static void on_keyboard(unsigned char key, int x, int y);
-<<<<<<< HEAD
 static void initialize(void);
 static void on_reshape(int width, int height);
+static void on_timer(int value);
 
 static int w_width, h_height;
 
-=======
-
-
-//proba git-a
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
 int main(int argc, char **argv)
 {
 	/*inicijalizuje glut*/
@@ -21,25 +24,22 @@ int main(int argc, char **argv)
 
 	/*rgb - rezim rgb boja,
 	double- iscrtava na jednom prozoru, pa tu sliku prebacuje na ekran-drugi prozor*/
-	glutInitDisplayMode(GLUT_RGB | /*GLUTH_DEPTH*/ GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
  
 	/*kreiranje prozora*/
-<<<<<<< HEAD
-	glutInitWindowSize(800,600);
-=======
 	glutInitWindowSize(1450,650);
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
 	glutInitWindowPosition(100,100);
 	glutCreateWindow(argv[0]);
 
 	/*poziv funkcije za prikaz prozora*/
 	glutDisplayFunc(on_display);	
 	glutKeyboardFunc(on_keyboard);
-<<<<<<< HEAD
         glutReshapeFunc(on_reshape);
 	
-	initialize();
+	hours=0;
+	timer_active=0;
 
+	initialize();
 
 	glutMainLoop();
 	return 0;
@@ -50,37 +50,18 @@ static void initialize(void)
 	/*boja pozadine*/
 	glClearColor(0.75,0,0.80,0);	
 
+	glEnable(GL_DEPTH_TEST);
 	glLineWidth(2);
 	/*bafer dubine*/
-	glEnable(GL_DEPTH_TEST);
+
 
 }
 
 static void on_reshape(int width,int height)
 {
-	w_width=width;
-	h_height=height;
-=======
+	//postavlja se viewport
+	glViewport(0, 0, width, height);
 	
-	glClearColor(0.75,0,0.80,0);	
-	/*bafer dubine*/
-	glEnable(GL_DEPTH_TEST);
-
-
-	glutMainLoop();
-	return 0;
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
-}
-
-static void on_display(void)
-{
-	/*brise se prethodni sadrzaj prozora,ali nije zamenjen*/
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-<<<<<<< HEAD
-	
-	/*definise pogled, kamera je u 0,0 koordinatama-donji-levi ugao,w i h sirina i visina prozora*/
-	glViewport(0,0,w_width,h_height);
-
 	/*podesavanje projekcije*/
 	glMatrixMode(GL_PROJECTION);
 
@@ -89,8 +70,36 @@ static void on_display(void)
 	
 	/*postavalja parametre perspektive*/
 	/*60-ugao vidnog polja, u pravcu y ose,  razlomak- vidno polje po x osi i kolicnik je sirine i visine,*/
-	gluPerspective(60, w_width/(float)h_height, 1, 5);/*1-bliza ravan - near, 5 dalja ravan -far*/
+	gluPerspective(60, width/(float)height, 1, 5);
+	/*1-bliza ravan - near, 5 dalja ravan -far*/
 
+}
+
+static void on_timer(int value)
+{
+    // Proverava se da li callback dolazi od odgovarajuceg tajmera. 
+    if (value != 0)
+        return;
+
+    // Azurira se vreme simulacije. 
+    hours += 6;
+
+    // Forsira se ponovno iscrtavanje prozora. 
+    glutPostRedisplay();
+
+    // Po potrebi se ponovo postavlja tajmer. 
+    if (timer_active)
+        glutTimerFunc(50, on_timer, 0);
+}
+
+static void on_display(void)
+{
+
+	float rotacija_kocke;
+	/*brise se prethodni sadrzaj prozora,ali nije zamenjen*/
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	/*definise pogled, kamera je u 0,0 koordinatama-donji-levi ugao,w i h sirina i visina prozora*/
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
@@ -99,39 +108,45 @@ static void on_display(void)
 	gluLookAt(1,2,3,0,0,0,0,1,0);
 
 	/*da se odnosi samo na objekat izmedju push i pop*/
+	rotacija_kocke = 360 * hours / (15 * 24);
 	glPushMatrix();
+   	  glRotatef(rotacija_kocke,0,1,0);		
 	  glColor3f(0,0,1);
 	  glutWireCube(1);		
 	glPopMatrix();
 
 
 	glutSwapBuffers();
-=======
-	glutSwapBuffers();       
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
+
 }
 
 static void on_keyboard(unsigned char key, int x, int y)
 {
 	switch(key)
 	{
-<<<<<<< HEAD
+
    	  case 27:
 		exit(0);
 		break;
-	}
 
-=======
-	case 27:
-		exit(0); 	
-		break;
-	}
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
+	  case 'g':
+   	  case 'G':
+        	//Pokrece se simulacija. 
+           if (!timer_active) 
+	    {
+            	glutTimerFunc(50, on_timer, 0);
+            	timer_active = 1;
+            }
+           break;
+
+    	case 's':
+    	case 'S':
+    	    // Zaustavlja se simulacija. 
+    	    timer_active = 0;
+         break;
+        }
 }
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> bd7b497d58433ac1b72152b3a2bbf20122a89faf
